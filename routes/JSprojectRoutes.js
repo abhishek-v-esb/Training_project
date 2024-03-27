dc241b4296f76fda198ccb81cdceda2d107d479f
abhishek-v-esb <abhishek.verma.2024.training@gmail.com>
2024-03-27 14:43:28 +0530

    JS2_functions_grid completed

--- routes/JSprojectRoutes.js
+++ routes/JSprojectRoutes.js
@@ -4,5 +4,8 @@ const routes = require("express").Router();
 routes.get("/dynamicGrid", authorization.authorization, (req, res) => {
   res.render("pages/JSproject/JS1_dynamic_grid");
 });
+routes.get("/functionGrid", authorization.authorization, (req, res) => {
+  res.render("pages/JSproject/JS2_functions_grid");
+});
 
 module.exports = routes;
